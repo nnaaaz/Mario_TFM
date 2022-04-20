e418a77ecbd20089e8daf25314e774583617b2ae
Pshy0 <Pshy0@users.noreply.github.com>
2022-04-20 15:43:19 +0200

    fixes

--- lua/nnaaaz_mario2.lua
+++ lua/nnaaaz_mario2.lua
@@ -5,10 +5,14 @@
 -- @author TFM:Nnaaaz#0000 (map, lua script)
 -- @author TFM:Pshy#3752 DC:Pshy#7998 (lua script)
 --
+-- @require pshy_bonuses_basic.lua
 -- @require pshy_bonuses_mario.lua
 -- @require pshy_checkpoints.lua
 -- @require pshy_essentials.lua
 -- @require pshy_loadersync.lua
+-- @require pshy_mapdb.lua
+-- @require pshy_mapinfo.lua
+-- @require pshy_newgame.lua
 -- @require pshy_players.lua
 -- @require pshy_scores.lua
 -- @require pshy_splashscreen.lua
@@ -202,7 +206,7 @@ end
 
 function eventInit()
 	for player_name in pairs(tfm.get.room.playerList) do
-		TouchPlayer
+		TouchPlayer(player_name)
 	end
 	pshy.newgame_ChatCommandRotc(user, "mario2") -- TODO: have a function for setting the rotation in `pshy_newgame.lua`!
 	tfm.exec.newGame("mario2")
