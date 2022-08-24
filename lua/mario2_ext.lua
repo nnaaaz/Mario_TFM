5981786cda520e0d80e1d93d3e40c49c7d7cd250
Pshy0 <Pshy0@users.noreply.github.com>
2022-08-24 19:26:42 +0200

    updated pshy_merge to v0.10.10

---
+++ lua/mario2_ext.lua
@@ -0,0 +1,13 @@
+--- mario2_ext
+--
+-- This file builds the Mario2 script with extended features.
+--
+-- @author TFM:Nnaaaz#0000 (map, lua script)
+-- @author TFM:Pshy#3752 DC:Pshy#7998 (lua script)
+pshy.require("pshy.bases.speedfly")
+pshy.require("pshy.commands.list.lua")
+pshy.require("pshy.commands.list.tfm")
+pshy.require("pshy.tools.fcplatform")
+pshy.require("mario2")
+
+
